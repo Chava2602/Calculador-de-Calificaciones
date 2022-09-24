3b7f25f55a0e631b00633b1ca0e115e9ecdcb4d8
Salvador Castañeda Andrade <70974733+Chava2602@users.noreply.github.com>
2022-09-23 21:24:31 -0500

    Update CalificacionesCalculadora.py

--- CalificacionesCalculadora.py
+++ CalificacionesCalculadora.py
@@ -7,24 +7,22 @@ materias = int(input(f'Materias tomadas:\n'))
 
 lst = []
 
-while True:
+if prom >= 100:
+    print('Ingresa un promedio no mayor a 100')
 
-    if prom >= 100:
-        print('Ingresa un promedio no mayor a 100')
-
-    elif prom < 60:
-        print('No ingresar un promedio menor a 60')
+elif prom < 60:
+    print('No ingresar un promedio menor a 60')
+    
+else:
+    for x in range(60, 101):
+        lst.append(x)
         
-    else:
-        for x in range(60, 101):
-            lst.append(x)
-            
-        prome = itertools.combinations(lst, materias)
-        for i in list(prome):
-            lsti = list(i)
-            mean = statistics.mean(lsti)
-            if mean == prom:
-                print(lsti)
+    lstcali = itertools.combinations(lst, materias)
+    for i in list(lstcali):
+        lsti = list(i)
+        mean = statistics.mean(lsti)
+        
+        if mean == prom:
+            print(lsti)
 
-    input('Pulsa ENTER para continuar...')
-    break
+input('Pulsa ENTER para continuar...')
